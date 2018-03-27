c5e89f9de8f23550de1634d24169d538e30e4c14
Denny Tek <tek.denny@gmail.com>
2018-03-27 12:36:28 -0400

    Separate js into modules
    Initialize separate js modules

---
+++ source/_js/footer.js
@@ -0,0 +1,13 @@
+const Footer = {
+  general() {
+    // Prevent default on footer close
+    $('footer .js-close-modal').on('click', e => {
+      e.preventDefault();
+    });
+  },
+  init() {
+    this.general();
+  }
+};
+
+Footer.init();
