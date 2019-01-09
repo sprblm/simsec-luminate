e61d17c96eb6bf8f5e3ef4abd86117260cb2b349
Denny Tek <tek.denny@gmail.com>
2019-01-09 17:45:23 -0500

    Add watch tasks to gulpfile

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -10,21 +10,21 @@ const webpackProdConfig = require('../../webpack-prod.config.js');
 // sourcemaps, concat, uglify;
 
 gulp.task('build:scripts:dev', () =>
-  gulp
-    .src('source/_js/scripts.js')
-    .pipe(plumber())
-    .pipe(webpack(webpackDevConfig))
-    .pipe(gulp.dest('source/assets/'))
-    .pipe(gulp.dest('_site/assets/'))
-    .on('error', gutil.log)
+    gulp
+        .src('source/_js/scripts.js')
+        .pipe(plumber())
+        .pipe(webpack(webpackDevConfig))
+        .pipe(gulp.dest('source/assets/'))
+        .pipe(gulp.dest('_site/assets/'))
+        .on('error', gutil.log)
 );
 
 gulp.task('build:scripts:prod', () =>
-  gulp
-    .src('source/_js/scripts.js')
-    .pipe(plumber())
-    .pipe(webpack(webpackProdConfig))
-    .pipe(gulp.dest('source/assets/'))
-    .pipe(gulp.dest('_site/assets/'))
-    .on('error', gutil.log)
+    gulp
+        .src('source/_js/scripts.js')
+        .pipe(plumber())
+        .pipe(webpack(webpackProdConfig))
+        .pipe(gulp.dest('source/assets/'))
+        .pipe(gulp.dest('_site/assets/'))
+        .on('error', gutil.log)
 );
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,14 +1,23 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 
-module.exports = () => {
-  // ///////////////////////// WATCH TASKS ///////////////////////////
-  gulp.task('build:jekyll:watch', gulp.series('build:jekyll:dev'), callback => {
-    browserSync.reload();
-    callback();
-  });
-  gulp.task('build:scripts:watch', gulp.series('build:scripts:dev'), callback => {
-    browserSync.reload();
-    callback();
-  });
-};
+// module.exports = () => {
+//   // ///////////////////////// WATCH TASKS ///////////////////////////
+//   gulp.task('build:jekyll:watch', gulp.series('build:jekyll:dev'), callback => {
+//     browserSync.reload();
+//     callback();
+//   });
+//   gulp.task('build:scripts:watch', gulp.series('build:scripts:dev'), callback => {
+//     browserSync.reload();
+//     callback();
+//   });
+// };
+
+// gulp.task('build:jekyll:watch', gulp.series('build:jekyll:dev'), callback => {
+//   browserSync.reload();
+//   callback();
+// });
+// gulp.task('build:scripts:watch', gulp.series('build:scripts:dev'), callback => {
+//   browserSync.reload();
+//   callback();
+// });
