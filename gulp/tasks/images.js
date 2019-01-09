92131d94d688110231885c454b9a82e6ba7e55d1
Denny Tek <tek.denny@gmail.com>
2019-01-09 12:49:02 -0500

    Migrate image task

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,12 +4,11 @@ const imagemin = require('gulp-imagemin');
 
 // ///////////////////////// IMAGE TASKS ///////////////////////////
 // Optimize tasks
-module.exports = () => {
-  gulp.task('build:images', () =>
-    gulp
-      .src('source/images/*')
-      .pipe(changed('_site/images'))
-      .pipe(imagemin())
-      .pipe(gulp.dest('_site/images'))
-  );
-};
+
+gulp.task('build:images', () =>
+  gulp
+    .src('source/images/*')
+    .pipe(changed('_site/images'))
+    .pipe(imagemin())
+    .pipe(gulp.dest('_site/images'))
+);
