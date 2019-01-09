c8a91b67f5a12eee6fdabd1740b99a986a663eea
Denny Tek <tek.denny@gmail.com>
2019-01-09 12:49:59 -0500

    Migrate scripts task

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -9,24 +9,22 @@ const webpackProdConfig = require('../../webpack-prod.config.js');
 // ///////////////////////// JAVASCRIPT TASKS ///////////////////////////
 // sourcemaps, concat, uglify;
 
-module.exports = () => {
-  gulp.task('build:scripts:dev', () =>
-    gulp
-      .src('source/_js/scripts.js')
-      .pipe(plumber())
-      .pipe(webpack(webpackDevConfig))
-      .pipe(gulp.dest('source/assets/'))
-      .pipe(gulp.dest('_site/assets/'))
-      .on('error', gutil.log)
-  );
+gulp.task('build:scripts:dev', () =>
+  gulp
+    .src('source/_js/scripts.js')
+    .pipe(plumber())
+    .pipe(webpack(webpackDevConfig))
+    .pipe(gulp.dest('source/assets/'))
+    .pipe(gulp.dest('_site/assets/'))
+    .on('error', gutil.log)
+);
 
-  gulp.task('build:scripts:prod', () =>
-    gulp
-      .src('source/_js/scripts.js')
-      .pipe(plumber())
-      .pipe(webpack(webpackProdConfig))
-      .pipe(gulp.dest('source/assets/'))
-      .pipe(gulp.dest('_site/assets/'))
-      .on('error', gutil.log)
-  );
-};
+gulp.task('build:scripts:prod', () =>
+  gulp
+    .src('source/_js/scripts.js')
+    .pipe(plumber())
+    .pipe(webpack(webpackProdConfig))
+    .pipe(gulp.dest('source/assets/'))
+    .pipe(gulp.dest('_site/assets/'))
+    .on('error', gutil.log)
+);
