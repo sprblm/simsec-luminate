1c86cec59d6889a9f5fecdc074659d131ee96333
Denny Tek <tek.denny@gmail.com>
2019-01-11 13:58:02 -0500

    Move all tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
+/* =========================================
+  gulp plugins
+========================================= */
+
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync');
-const gulp = require('gulp');
+const changed = require('gulp-changed');
 const htmlmin = require('gulp-htmlmin');
-const HubRegistry = require('gulp-hub');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const imagemin = require('gulp-imagemin');
+const importCss = require('gulp-import-css');
+const minifyCss = require('gulp-minify-css');
+const mocha = require('gulp-spawn-mocha');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const shell = require('gulp-shell');
+const webpack = require('webpack-stream');
+const webpackDevConfig = require('./webpack-dev.config.js');
+const webpackProdConfig = require('./webpack-prod.config.js');
 
-// For Development: `gulp` will run `gulp serve`, to build and watch
-
-// require('./gulp/tasks/html');
-
-// require('./gulp/tasks/deploy')();
-// require('./gulp/tasks/images')();
-// require('./gulp/tasks/scripts')();
-// require('./gulp/tasks/serve')();
-// require('./gulp/tasks/styles')();
-// require('./gulp/tasks/tests')();
-// require('./gulp/tasks/watch')();
-// require('./gulp/tasks/build_dev')();
-// require('./gulp/tasks/build_prod')();
-
-// DEFAULT
-// gulp.task('default', gulp.series('serve'));
+/* =========================================
+  html
+========================================= */
 
 gulp.task('build:html', () =>
   gulp
@@ -27,46 +31,133 @@ gulp.task('build:html', () =>
     .pipe(gulp.dest('_site/'))
 );
 
-/* load some files into the registry */
-const hub = new HubRegistry(['gulp/tasks/html.js', 'gulp/tasks/*.js']);
+/* =========================================
+  styles
+========================================= */
 
-/* tell gulp to use the tasks just loaded */
-gulp.registry(hub);
+gulp.task('build:styles', () =>
+  gulp
+    .src('source/_sass/styles.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(importCss())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions', '> 5%', 'IE 9']
+      })
+    )
+    .pipe(minifyCss({ keepBreaks: false }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('source/assets/'))
+    .pipe(gulp.dest('_site/assets/'))
+    .pipe(browserSync.stream())
+    .on('error', gutil.log)
+);
 
-gulp.task('list', done => {
-  console.log(hub);
-  let tasks = Object.keys(gulp.registry().tasks());
-  console.log(JSON.stringify(tasks), null, 2);
-  done();
+/* =========================================
+  images
+========================================= */
+
+gulp.task('build:images', () =>
+  gulp
+    .src('source/images/*')
+    .pipe(changed('_site/images'))
+    .pipe(imagemin())
+    .pipe(gulp.dest('_site/images'))
+);
+
+/* =========================================
+  scripts
+========================================= */
+
+gulp.task('build:scripts:dev', () => {
+  return gulp
+    .src('source/_js/scripts.js')
+    .pipe(plumber())
+    .pipe(webpack(webpackDevConfig))
+    .pipe(gulp.dest('source/assets/'))
+    .pipe(gulp.dest('_site/assets/'))
+    .on('error', gutil.log);
 });
 
-// these tasks wont run in separate files due to loading order
-// BUILD_DEV
+gulp.task('build:scripts:prod', () =>
+  gulp
+    .src('source/_js/scripts.js')
+    .pipe(plumber())
+    .pipe(webpack(webpackProdConfig))
+    .pipe(gulp.dest('source/assets/'))
+    .pipe(gulp.dest('_site/assets/'))
+    .on('error', gutil.log)
+);
+
+/* =========================================
+ build dev
+========================================= */
+
+gulp.task('build:jekyll', shell.task('bundle exec jekyll build --incremental --config _config.yml,_config_dev.yml'));
+
 gulp.task('build:jekyll:dev', gulp.series('build:jekyll', 'build:html'));
 
 gulp.task('build:dev', gulp.series(gulp.series('build:scripts:dev', 'build:styles'), 'build:jekyll', 'build:html'));
 
-// BUILD_PROD
+/* =========================================
+  build prod
+========================================= */
+gulp.task('jekyll:clean', shell.task(['bundle exec jekyll clean']));
+
+gulp.task('build:jekyll:prod', shell.task('bundle exec jekyll build'));
+
 gulp.task(
   'build:prod',
   gulp.series('jekyll:clean', 'build:scripts:prod', 'build:styles', 'build:images', 'build:jekyll:prod', 'build:html')
 );
 
-// default serve
+/* =========================================
+  tests
+========================================= */
+
 gulp.task(
-  'serve',
-  gulp.series('build:dev', () => {
-    browserSync.init({
-      server: '_site',
-      ghostMode: false,
-      logFileChanges: true,
-      open: false
-    });
-    gulp.watch('source/**/*.scss', ['build:styles']);
-    gulp.watch('source/**/*.js', ['build:scripts:watch']);
-    gulp.watch(['source/**/*.html', '!_site/**/*.*'], ['build:jekyll:watch']);
-    gulp.watch('_data/**.*+(yml|yaml|csv|json)', ['build:jekyll:watch']);
-  })
+  'html_proofer',
+  shell.task([
+    'bundle exec htmlproofer ./_site --allow-hash-href --disable-external --check-favicon --url-swap "/objectively-jekyll-boilerplate|:" --check-html'
+  ])
 );
 
-gulp.task('default', gulp.series('serve'));
+gulp.task('test:html', gulp.series('build:jekyll:dev', 'html_proofer'));
+
+gulp.task('test:es-lint', shell.task(['eslint ./source/_js/**/*.js']));
+
+gulp.task('test:mocha', () => gulp.src('spec/**/*.spec.js').pipe(mocha()));
+
+gulp.task('test', done => {
+  gulp.parallel('test:html', 'test:es-lint', 'test:mocha');
+  done();
+});
+
+/* =========================================
+  serve
+========================================= */
+
+const server = browserSync.create();
+
+function serve(done) {
+  server.init({
+    server: '_site',
+    ghostMode: false,
+    logFileChanges: true,
+    open: false,
+    watchOptions: {
+      awaitWriteFinish: true
+    }
+  });
+  done();
+}
+
+const watch = () => {
+  gulp.watch('source/**/*.scss', gulp.series('build:styles'));
+  gulp.watch('source/**/*.js', gulp.series('build:scripts:dev'));
+  gulp.watch(['source/**/*.html', '!_site/**/*.*'], gulp.series('build:jekyll:dev'));
+  gulp.watch('_data/**.*+(yml|yaml|csv|json)', gulp.series('build:jekyll:dev'));
+};
+
+gulp.task('default', gulp.series(serve, 'build:dev', watch));
