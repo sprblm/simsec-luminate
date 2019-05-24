0dac5a79927471c99883e2226111872545dcf427
enabek <enabek@gmail.com>
2019-05-24 19:12:38 +0900

    Toggle filters on recommendations

--- source/_js/recommendations-filter.js
+++ source/_js/recommendations-filter.js
@@ -46,7 +46,7 @@ const RecommendationsFilter = {
           }
         });
         RecommendationsFilter.filterList(RecommendationsFilter.searchQueries);
-        RecommendationsFilter.noResultsDiv();
+        // RecommendationsFilter.noResultsDiv();
       });
     });
   },
@@ -66,7 +66,28 @@ const RecommendationsFilter = {
     RecommendationsFilter.searchQueries[searchQuery] = searchParam;
     RecommendationsFilter.filterList(RecommendationsFilter.searchQueries);
     RecommendationsFilter.matchSearchQueriesToUI();
-    RecommendationsFilter.noResultsDiv();
+    // RecommendationsFilter.noResultsDiv();
+  },
+  toggleSwitch: () => {
+    const switchControl = document.querySelector('.mdc-switch');
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-switch'));
+
+    switchControl.addEventListener('click', () => {
+      switchControl.classList.toggle('mdc-switch--checked');
+
+      if (switchControl.classList.contains('mdc-switch--checked')) {
+        document.querySelector('.insight-dropdown').style.display = 'block';
+        document.querySelector('.theme-dropdown').style.display = 'none';
+      } else {
+        document.querySelector('.insight-dropdown').style.display = 'none';
+        document.querySelector('.theme-dropdown').style.display = 'block';
+      }
+
+      RecommendationsFilter.setSearchQueryDefaults();
+      RecommendationsFilter.recommendationList.filter();
+      RecommendationsFilter.recommendationList.sort('title', { order: 'asc' });
+      document.querySelectorAll('.dropdown').forEach(dropdown => dropdown.selectedIndex = 0);
+    });
   },
   matchSearchQueriesToUI() {
     const dropdowns = document.querySelectorAll('.dropdown');
@@ -82,20 +103,20 @@ const RecommendationsFilter = {
 
     RecommendationsFilter.filterList(RecommendationsFilter.searchQueries);
   },
-  noResultsDiv: () => {
-    const recommendationsCount = document.querySelectorAll('.list__item').length;
-
-    recommendationsCount > 0
-    ? document.querySelector('.no-results').style.display = 'none'
-    : document.querySelector('.no-results').style.display = 'block';
-  },
+  // noResultsDiv: () => {
+  //   const recommendationsCount = document.querySelectorAll('.list__item').length;
+  //
+  //   recommendationsCount > 0
+  //   ? document.querySelector('.no-results').style.display = 'none'
+  //   : document.querySelector('.no-results').style.display = 'block';
+  // },
   clearAllDropdowns: () => {
     document.querySelector('.clear-all').addEventListener('click', e => {
       e.preventDefault();
       RecommendationsFilter.setSearchQueryDefaults();
       RecommendationsFilter.recommendationList.filter();
       RecommendationsFilter.recommendationList.sort('title', { order: 'asc' });
-      RecommendationsFilter.noResultsDiv();
+      // RecommendationsFilter.noResultsDiv();
       document.querySelectorAll('.dropdown').forEach(dropdown => dropdown.selectedIndex = 0);
     });
   },
@@ -107,6 +128,7 @@ const RecommendationsFilter = {
       this.filterByDropdowns();
       this.filterByUrlParams();
       this.clearAllDropdowns();
+      this.toggleSwitch();
     }
   }
 }
