bbe0354f1f01a481b4dfc5689810c44d8a8ce58e
Denny Tek <tek.denny@gmail.com>
2019-06-06 17:41:31 -0400

    Resize figure images

--- source/_plugins/ui-templates.rb
+++ source/_plugins/ui-templates.rb
@@ -6,15 +6,15 @@ module UITemplates
 
     chart_title = "<div class='figure-number'>Figure #{number}</div> <div class='figure-title'>#{title}</div>"
     chart_description_path = get_relative_url("/figures.html#figure-#{number}")
-    chart_img_html = "<img src='#{img_src}' alt='#{alt_text}' longdesc='#{chart_description_path}' />"
+    chart_img_html = "<div class='figure-image-container'> <img src='#{img_src}' alt='#{alt_text}' longdesc='#{chart_description_path}' /></div>"
     chart_caption = "<figcaption>#{caption}</figcaption>"
     chart_description_link = "<div class='figure-link-container'><a class='figure-description-link' href='#{chart_description_path}'>Text description of chart</a></div>"
 
     chartHtml = [
       "<div class='chart-container'><figure>",
       chart_title,
-      chart_img_html,
       chart_caption,
+      chart_img_html,
       chart_description_link,
       "</figure></div>"
     ].join('')
