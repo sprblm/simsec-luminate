8cc17b528ce0971cf96e1e862f7f9f7079fb77c6
Denny Tek <tek.denny@gmail.com>
2019-06-21 16:44:09 -0400

    Update 2019-06-21T20:44:07.941Z

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jquery: true
-  }
-};
--- source/_js/utils.js
+++
@@ -1,9 +0,0 @@
-const Utils = {
-  markdownLinksNewPage: () => {
-    $('.markdown a').map((idx, link) => {
-      return link.setAttribute('target', '_blank');
-    });
-  }
-};
-
-module.exports = Utils;
